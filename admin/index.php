789f530bed47b5537ac4dcffae34ee6469f71713
Paul Twomey <twomeypm@beloit.edu>
2016-10-31 20:00:02 -0500

    minor change to test oddity on commit

--- admin/index.php
+++ admin/index.php
@@ -1,14 +1,16 @@
+
+
 <!DOCTYPE html>
 <html lang="en">
 
 <?php
-
+//minor change to allow a commit
 // Access to global variables
 require_once ('../global/include.php');
 
 // Make sure user is allowed to view admin area
 if (! $isAdmin) {
-	header ( 'Location: ' . SYSTEM_WEB_BASE_ADDRESS . 'admin/login.php' );
+    header ( 'Location: ' . SYSTEM_WEB_BASE_ADDRESS . 'admin/login.php' );
 }
 
 require ("php/head.php");
@@ -27,177 +29,177 @@ $activityCount = $row ['count'];
 
 <body>
 
-	<div id="wrapper">
+    <div id="wrapper">
 
-		<?php require("php/sidebar.php"); echoNav($db, "index"); ?>
+        <?php require("php/sidebar.php"); echoNav($db, "index"); ?>
 
-		<div id="page-wrapper">
+        <div id="page-wrapper">
 
-			<div class="row">
-				<div class="col-lg-12">
-					<div class="row">
-						<div class="col-md-8">
-							<h1>
-								Dashboard <small>Overview</small>
-							</h1>
-						</div>
-						<div class="col-md-4">
-							<button type="button" style="float:left; margin-top: 20px; margin-bottom: 10px;" class="btn btn-primary btn-lg" onClick="window.open('<?php echo SYSTEM_WEB_BASE_ADDRESS."user/generalinstruction.php"?>')"><span class="glyphicon glyphicon-log-in"></span></button>
-							<button style="float:right; margin-top: 20px; margin-bottom: 10px;" type="button" class="btn btn-primary btn-lg" onClick="window.open('printinstructions.php')">Print Instructions</button>
-						</div>
+            <div class="row">
+                <div class="col-lg-12">
+                    <div class="row">
+                        <div class="col-md-8">
+                            <h1>
+                                Dashboard <small>Overview</small>
+                            </h1>
+                        </div>
+                        <div class="col-md-4">
+                            <button type="button" style="float:left; margin-top: 20px; margin-bottom: 10px;" class="btn btn-primary btn-lg" onClick="window.open('<?php echo SYSTEM_WEB_BASE_ADDRESS."user/generalinstruction.php"?>')"><span class="glyphicon glyphicon-log-in"></span></button>
+                            <button style="float:right; margin-top: 20px; margin-bottom: 10px;" type="button" class="btn btn-primary btn-lg" onClick="window.open('printinstructions.php')">Print Instructions</button>
+                        </div>
 
-											</div>
-					<ol class="breadcrumb">
-						<li class="active"><i class="fa fa-dashboard"></i> Dashboard</li>
-					</ol>
-				</div>
-			</div>
-			<!-- /.row -->
+                                            </div>
+                    <ol class="breadcrumb">
+                        <li class="active"><i class="fa fa-dashboard"></i> Dashboard</li>
+                    </ol>
+                </div>
+            </div>
+            <!-- /.row -->
 
-			<div class="row">
-				<div class="col-lg-6">
-					<div class="panel panel-info">
-						<div class="panel-heading">
-							<div class="row">
-								<div class="col-xs-6">
-									<i class="fa fa-users fa-5x"></i>
-								</div>
-								<div class="col-xs-6 text-right">
-									<p class="announcement-heading"><?php echo $studentCount; ?></p>
-									<p class="announcement-text">Volunteers</p>
-								</div>
-							</div>
-						</div>
-						<a href="volunteer.php">
-							<div class="panel-footer announcement-bottom">
-								<div class="row">
-									<div class="col-xs-6">View Volunteers</div>
-									<div class="col-xs-6 text-right">
-										<i class="fa fa-arrow-circle-right"></i>
-									</div>
-								</div>
-							</div>
-						</a>
-					</div>
-				</div>
+            <div class="row">
+                <div class="col-lg-6">
+                    <div class="panel panel-info">
+                        <div class="panel-heading">
+                            <div class="row">
+                                <div class="col-xs-6">
+                                    <i class="fa fa-users fa-5x"></i>
+                                </div>
+                                <div class="col-xs-6 text-right">
+                                    <p class="announcement-heading"><?php echo $studentCount; ?></p>
+                                    <p class="announcement-text">Volunteers</p>
+                                </div>
+                            </div>
+                        </div>
+                        <a href="volunteer.php">
+                            <div class="panel-footer announcement-bottom">
+                                <div class="row">
+                                    <div class="col-xs-6">View Volunteers</div>
+                                    <div class="col-xs-6 text-right">
+                                        <i class="fa fa-arrow-circle-right"></i>
+                                    </div>
+                                </div>
+                            </div>
+                        </a>
+                    </div>
+                </div>
 
-				<div class="col-lg-6">
-					<div class="panel panel-success">
-						<div class="panel-heading">
-							<div class="row">
-								<div class="col-xs-6">
-									<i class="fa fa-th-list fa-5x"></i>
-								</div>
-								<div class="col-xs-6 text-right">
-									<p class="announcement-heading"><?php echo $activityCount; ?></p>
-									<p class="announcement-text">Activities</p>
-								</div>
-							</div>
-						</div>
-						<a href="activity.php">
-							<div class="panel-footer announcement-bottom">
-								<div class="row">
-									<div class="col-xs-6">View Activities</div>
-									<div class="col-xs-6 text-right">
-										<i class="fa fa-arrow-circle-right"></i>
-									</div>
-								</div>
-							</div>
-						</a>
-					</div>
-				</div>
-			</div>
-			<!-- /.row -->
+                <div class="col-lg-6">
+                    <div class="panel panel-success">
+                        <div class="panel-heading">
+                            <div class="row">
+                                <div class="col-xs-6">
+                                    <i class="fa fa-th-list fa-5x"></i>
+                                </div>
+                                <div class="col-xs-6 text-right">
+                                    <p class="announcement-heading"><?php echo $activityCount; ?></p>
+                                    <p class="announcement-text">Activities</p>
+                                </div>
+                            </div>
+                        </div>
+                        <a href="activity.php">
+                            <div class="panel-footer announcement-bottom">
+                                <div class="row">
+                                    <div class="col-xs-6">View Activities</div>
+                                    <div class="col-xs-6 text-right">
+                                        <i class="fa fa-arrow-circle-right"></i>
+                                    </div>
+                                </div>
+                            </div>
+                        </a>
+                    </div>
+                </div>
+            </div>
+            <!-- /.row -->
 
-			<div class="row">
-				<div class="col-lg-6">
-					<div class="panel panel-primary">
-						<div class="panel-heading">
-							<p class="pull-right">Click toggles to flip lock</p>
-							<h3 class="panel-title">
-								<i class="fa fa-lock"></i> System Locks
-							</h3>
-						</div>
-						<div class="panel-body">
+            <div class="row">
+                <div class="col-lg-6">
+                    <div class="panel panel-primary">
+                        <div class="panel-heading">
+                            <p class="pull-right">Click toggles to flip lock</p>
+                            <h3 class="panel-title">
+                                <i class="fa fa-lock"></i> System Locks
+                            </h3>
+                        </div>
+                        <div class="panel-body">
 
-							<?php
+                            <?php
 
-							$result = $db->query ( "SELECT * FROM system_locks" );
-							$affected_rows = mysqli_num_rows ( $result );
+                            $result = $db->query ( "SELECT * FROM system_locks" );
+                            $affected_rows = mysqli_num_rows ( $result );
 
-							for($i = 0; $i < $affected_rows; $i ++) {
-								$row = $result->fetch_assoc ();
+                            for($i = 0; $i < $affected_rows; $i ++) {
+                                $row = $result->fetch_assoc ();
 
-								?>
+                                ?>
 
-							<div class="col-md-6">
-								<h4 style="margin: 0px; margin-bottom: 5px">
-									<?php echo $row['name']; ?>
-									<small><?php echo $row['description']; ?> </small>
-								</h4>
-								<div id="lock" lock_id="<?php echo $row['system_lock_id']; ?>">
-									<div class="make-switch switch-small" data-on="success"
-										data-off="danger" data-on-label="<i
-
-
-										class='fa fa-unlock'>
-										</i>" data-off-label="<i class='fa fa-lock'></i>"> <input
-											type="checkbox"
-											<?php echo $row['locked'] == "t" ? "" : "checked"; ?>>
-									</div>
-								</div>
-								<hr />
-							</div>
+                            <div class="col-md-6">
+                                <h4 style="margin: 0px; margin-bottom: 5px">
+                                    <?php echo $row['name']; ?>
+                                    <small><?php echo $row['description']; ?> </small>
+                                </h4>
+                                <div id="lock" lock_id="<?php echo $row['system_lock_id']; ?>">
+                                    <div class="make-switch switch-small" data-on="success"
+                                        data-off="danger" data-on-label="<i
 
 
-
-							<?php
-							}
-							?>
-
-						</div>
-					</div>
-				</div>
-
-
-			</div>
+                                        class='fa fa-unlock'>
+                                        </i>" data-off-label="<i class='fa fa-lock'></i>"> <input
+                                            type="checkbox"
+                                            <?php echo $row['locked'] == "t" ? "" : "checked"; ?>>
+                                    </div>
+                                </div>
+                                <hr />
+                            </div>
 
 
+
+                            <?php
+                            }
+                            ?>
+
+                        </div>
+                    </div>
+                </div>
+
+
+            </div>
 
 
 
 
-			<div class="row">
-				<div class="col-lg-6">
-					<div class="panel panel-primary">
-						<div class="panel-heading">
-							<p class="pull-right">Send email to volunteer groups</p>
-							<h3 class="panel-title">
-								<i class="fa fa-envelope"></i> Group Email
-							</h3>
-						</div>
-						<div class="panel-body">
 
 
-	<?php
+            <div class="row">
+                <div class="col-lg-6">
+                    <div class="panel panel-primary">
+                        <div class="panel-heading">
+                            <p class="pull-right">Send email to volunteer groups</p>
+                            <h3 class="panel-title">
+                                <i class="fa fa-envelope"></i> Group Email
+                            </h3>
+                        </div>
+                        <div class="panel-body">
+
+
+    <?php
 
 // Query to find students who can come to dinner
 
 $queries = array(
 
-		#query for shift soon but not checked in with us
+        #query for shift soon but not checked in with us
 
-		#query for missed shift
+        #query for missed shift
 
-		"SELECT email FROM students where profile_complete = 'f';"
-			=> "Have not finished basic profile.",
+        "SELECT email FROM students where profile_complete = 'f';"
+            => "Have not finished basic profile.",
 
-		"SELECT email FROM students where times_complete = 'f' and profile_complete = 't';"
-			=> "Have not given available times.",
+        "SELECT email FROM students where times_complete = 'f' and profile_complete = 't';"
+            => "Have not given available times.",
 
 
-		"SELECT email FROM students where times_complete = 't' and profile_complete = 't';"
-			=> "who completely registered for sending notes",
+        "SELECT email FROM students where times_complete = 't' and profile_complete = 't';"
+            => "who completely registered for sending notes",
 
 
 
@@ -207,21 +209,21 @@ $queries = array(
 
 
 ?>
-	<div>
+    <div>
   <div class="row">
     <div class="col-sm-9"><select class="form-control" id="mass_email_query_selection">
 <option value= '-1' >Select a group to send email</option>
 <?php
 $query_number=1;
-				foreach ($queries as $query => $desc) {
-					?>
-								<option value= '<?php echo $query_number?>' ><?php echo $desc?></option>
+                foreach ($queries as $query => $desc) {
+                    ?>
+                                <option value= '<?php echo $query_number?>' ><?php echo $desc?></option>
 
-					<?php $query_number++;
-				}
-					?>
+                    <?php $query_number++;
+                }
+                    ?>
 
-  </select>	</div>
+  </select>    </div>
     <div class="col-sm-3"><button class="btn btn-primary form-control" id="edit_recipient_button" data-toggle="modal" data-target="#recipient_window">Edit Recipient</button></div>
   </div>
 
@@ -230,7 +232,7 @@ $query_number=1;
 <p></p>
 
 <div class ="modal fade" role="dialog" id ="recipient_window" data-backdrop="static",
-	    data-keyboard="false">
+        data-keyboard="false">
 <div class="modal-dialog">
 <div class="modal-content">
 <div class="modal-header">
@@ -251,26 +253,22 @@ $query_number=1;
 
 <div id="recipients_to_show"></div>
 
-  	<div id="mass_email">
-								Hello
+      <div id="mass_email">
+                                Hello
 
-								,<br /> <br /> <br />
-							</div>
-							<p></p>
-						<button id="send_mass_email"
-								type="button"
-								class="btn btn-primary pull-right">Send</button>
+                                ,<br /> <br /> <br />
+                            </div>
+                            <p></p>
+                        <button id="send_mass_email"
+                                type="button"
+                                class="btn btn-primary pull-right">Send</button>
 </div>
-						</div>
-					</div>
+                        </div>
+                    </div>
 
 
 
-			</div>
-
-
-
-
+            </div>
 
 
 
@@ -278,14 +276,18 @@ $query_number=1;
 
 
 
-		</div>
-		<p>&nbsp;</p>
-		<!-- /#page-wrapper -->
 
-		<?php require_once("footer.html"); ?>
 
-	</div>
-	<!-- /#wrapper -->
+
+
+        </div>
+        <p>&nbsp;</p>
+        <!-- /#page-wrapper -->
+
+        <?php require_once("footer.html"); ?>
+
+    </div>
+    <!-- /#wrapper -->
 
 </body>
 </html>
