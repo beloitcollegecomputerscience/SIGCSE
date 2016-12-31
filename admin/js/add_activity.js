e36217ce41d8b90a8f29107338e4b6d7771c3ae2
Yi Zhou <yizhou@Yis-MacBook-Pro.local>
2016-12-31 13:56:13 -0600

    1)When new activities are created the covering slots will be added into table covering_slot. 2)check for new activity name duplication

--- admin/js/add_activity.js
+++ admin/js/add_activity.js
@@ -1,215 +1,238 @@
- 
+
 $(document).ready(function(){
 
 var dup_organizer=false;
 var dup_room=false;
 var dup_type=false;
+var dup_name=false;
 
-	
-	var delay = (function(){
-	  var timer = 0;
-	  return function(callback, ms){
-	    clearTimeout (timer);
-	    timer = setTimeout(callback, ms);
-	  };
-	})();
+    var delay = (function(){
+      var timer = 0;
+      return function(callback, ms){
+        clearTimeout (timer);
+        timer = setTimeout(callback, ms);
+      };
+    })();
 
- 
- $('#other_activity_type').keyup(function(){ 
- 	
- 	delay(function(){
+  $('#activity_name').keyup(function(){
+
+  delay(function(){
      $.ajax({
              url: 'add_form.php',
              type: 'POST',
              data : {
-             	action : '1',
-             	other_activity_type : $("#other_activity_type").val()
-          }//when this is done the msg echoed from the php file that this file posts to which is add_form.php. 
+              action : '5',
+              activity_name : $("#activity_name").val()
+          }//when this is done the msg echoed from the php file that this file posts to which is add_form.php.
           }).done(function($msg) {
-        		if($msg.indexOf("duplicatetype")!= -1 ){
-        			bootbox.alert("duplicate type, change name or select from the list");
-        			dup_type = true;
-        		}else{dup_type=false;
-        		}
-          
-          });    
- 	},600);
+            if($msg.indexOf("duplicateactivityname")!= -1 ){
+              bootbox.alert("An activity of this name already exist. Change the name to create a new activity. Otherwise an activity of duplicate name will be created.");
+              dup_name=true;
+            }else{dup_name=false;}
+
+          });
+  },600);
+
+     });
+ $('#other_activity_type').keyup(function(){
+
+     delay(function(){
+     $.ajax({
+             url: 'add_form.php',
+             type: 'POST',
+             data : {
+                 action : '1',
+                 other_activity_type : $("#other_activity_type").val()
+          }//when this is done the msg echoed from the php file that this file posts to which is add_form.php.
+          }).done(function($msg) {
+                if($msg.indexOf("duplicatetype")!= -1 ){
+                    bootbox.alert("duplicate type, change name or select from the list");
+                    dup_type = true;
+                }else{dup_type=false;
+                }
+
+          });
+     },600);
 
      });
 
-    $('#other_activity_location').keyup(function(){ 
-    	
-    	delay(function(){
+    $('#other_activity_location').keyup(function(){
+
+        delay(function(){
         $.ajax({
                 url: 'add_form.php',
                 type: 'POST',
                 data : {
-                	action : '2',
-                	other_activity_location : $("#other_activity_location").val()
-             }//when this is done the msg echoed from the php file that this file posts to which is add_form.php. 
+                    action : '2',
+                    other_activity_location : $("#other_activity_location").val()
+             }//when this is done the msg echoed from the php file that this file posts to which is add_form.php.
              }).done(function($msg) {
-           		if($msg.indexOf("duplicateroom")!= -1 ){
-           			bootbox.alert("duplicate room name, change name or select from the list");
-           			dup_room=true;
-           		}else{dup_room=false;
-           		}
-             
-             });    
-    	},600);
+                   if($msg.indexOf("duplicateroom")!= -1 ){
+                       bootbox.alert("duplicate room name, change name or select from the list");
+                       dup_room=true;
+                   }else{dup_room=false;
+                   }
+
+             });
+        },600);
 
         });
-    
-   $('#other_activity_organizer_last').keyup(function(){ 
-    	
-    	delay(function(){
+
+   $('#other_activity_organizer_last').keyup(function(){
+
+        delay(function(){
         $.ajax({
                 url: 'add_form.php',
                 type: 'POST',
                 data : {
-                	action : '3',
-                	other_activity_organizer_first : $("#other_activity_organizer_first").val(),
-                	other_activity_organizer_last : $("#other_activity_organizer_last").val()
-             }//when this is done the msg echoed from the php file that this file posts to which is add_form.php. 
+                    action : '3',
+                    other_activity_organizer_first : $("#other_activity_organizer_first").val(),
+                    other_activity_organizer_last : $("#other_activity_organizer_last").val()
+             }//when this is done the msg echoed from the php file that this file posts to which is add_form.php.
              }).done(function($msg) {
-           		if($msg.indexOf("duplicateorganizer")!= -1 ){
-           			bootbox.alert("duplicate organizer, change name or select from the list");
-           			dup_organizer=true;
-           		}else{dup_organizer=false;
-           		}
-             
-             });    
-    	},600);
+                   if($msg.indexOf("duplicateorganizer")!= -1 ){
+                       bootbox.alert("duplicate organizer, change name or select from the list");
+                       dup_organizer=true;
+                   }else{dup_organizer=false;
+                   }
+
+             });
+        },600);
 
         });
-   $('#other_activity_organizer_first').keyup(function(){ 
-   	
-   	delay(function(){
+   $('#other_activity_organizer_first').keyup(function(){
+
+       delay(function(){
        $.ajax({
                url: 'add_form.php',
                type: 'POST',
                data : {
-               	action : '3',
-               	other_activity_organizer_first : $("#other_activity_organizer_first").val(),
-               	other_activity_organizer_last : $("#other_activity_organizer_last").val()
-            }//when this is done the msg echoed from the php file that this file posts to which is add_form.php. 
+                   action : '3',
+                   other_activity_organizer_first : $("#other_activity_organizer_first").val(),
+                   other_activity_organizer_last : $("#other_activity_organizer_last").val()
+            }//when this is done the msg echoed from the php file that this file posts to which is add_form.php.
             }).done(function($msg) {
-          		if($msg.indexOf("duplicateorganizer")!= -1 ){
-          			bootbox.alert("duplicate organizer, change name or select from the list");
-          			dup_organizer=true;
-          		}else{dup_organizer=false;;
-          		}
-            
-            });    
-   	},600);
+                  if($msg.indexOf("duplicateorganizer")!= -1 ){
+                      bootbox.alert("duplicate organizer, change name or select from the list");
+                      dup_organizer=true;
+                  }else{dup_organizer=false;;
+                  }
+
+            });
+       },600);
 
        });
-			 
-			 
-			 
-		
-		 
-		 
-		 
+
+
+
+
+
+
+
         $('#activity_type').change(function() {
              if (this.value == 'Other') {
                  $("#other_activity_type_form").show();
-      
-                 
+
+
              } else {
-            	 
-            	 $("#other_activity_type_form").hide();
-            	 $("#other_activity_type").val("");
-                 
+
+                 $("#other_activity_type_form").hide();
+                 $("#other_activity_type").val("");
+
                  dup_type=false;
-   
+
              }
          });
-         
+
          $('#activity_location').change(function() {
              if (this.value == 'Other') {
                  $("#other_activity_location_form").show();
-      
-                 
+
+
              } else {
                  $("#other_activity_location_form").hide();
                  $("#other_activity_location").val("");
                  dup_room=false;
-   
+
              }
          });
         $('#activity_organizer').change(function() {
              if (this.value == 'Other') {
                  $("#other_activity_organizer_form").show();
-      
-                 
+
+
              } else {
                  $("#other_activity_organizer_form").hide();
                  $("#other_activity_organizer_first").val("");
                  $("#other_activity_organizer_last").val("");
                  dup_organizer=false;
-   
+
              }
          });
-         
 
-	 
-	 
-	 
 
-	 //when the button with id add_activity_submit is clicked all the values of inputs from the add_activity.php are posted to add_form.php
-	 
+
+
+
+
+     //when the button with id add_activity_submit is clicked all the values of inputs from the add_activity.php are posted to add_form.php
+
     $('#add_activity_submit').click(function(){
-    	
-    	var missing =false;
-    	var error=false;
-    	var counter=0;
-    	var num_err=false;    
-    	if (dup_organizer){bootbox.alert("Please fix duplicate organizer, change name or select from the list"); error=true;} 
-    	if (dup_room){bootbox.alert("Please fix duplicate room, change room name or select from the list");error=true;} 
-    	if (dup_type){bootbox.alert("Please fix duplicate type, change type or select from the list");error=true;} 
-    	if($("#activity_end_time").val()<=$("#activity_start_time").val()){bootbox.alert("invalid time slot");error=true;}
-    	if(parseInt($("#min_num").val(),10)>parseInt($("#max_num").val(),10)){num_err=true; }
-    	else if(parseInt($("#min_num").val(),10)>parseInt($("#desired_num").val(),10)){num_err=true;}
-    	else if(parseInt($("#desired_num").val(),10)>parseInt($("#max_num").val(),10)){num_err=true;}
-    	if(num_err){bootbox.alert("error in volunteer numbers");error=true;}
-    	//check for missing required info
+
+        var missing =false;
+        var error=false;
+        var counter=0;
+        var num_err=false;
+        if (dup_organizer){bootbox.alert("Please fix duplicate organizer, change name or select from the list"); error=true;}
+        if (dup_room){bootbox.alert("Please fix duplicate room, change room name or select from the list");error=true;}
+        if (dup_type){bootbox.alert("Please fix duplicate type, change type or select from the list");error=true;}
+        if(dup_name){bootbox.alert("Please fix duplicate name.");error=true;}
+        if($("#activity_end_time").val()<=$("#activity_start_time").val()){bootbox.alert("invalid time slot");error=true;}
+        if(parseInt($("#min_num").val(),10)>parseInt($("#max_num").val(),10)){num_err=true; }
+        else if(parseInt($("#min_num").val(),10)>parseInt($("#desired_num").val(),10)){num_err=true;}
+        else if(parseInt($("#desired_num").val(),10)>parseInt($("#max_num").val(),10)){num_err=true;}
+        if(num_err){bootbox.alert("error in volunteer numbers");error=true;}
+        //check for missing required info
 
    $(".required:visible").each(function(){
-   		 if(!$(this).val() ){
- 			 $(this).css({'background-color' : '#ffefef'});
-    			 missing=true;
-   			 
-    			 }
-  		});
-    	if(missing){bootbox.alert("missing info");error=true;}
-    	if(!error){
-    		
-    		$.ajax({
+            if(!$(this).val() ){
+              $(this).css({'background-color' : '#ffefef'});
+                 missing=true;
+
+                 }
+          });
+        if(missing){bootbox.alert("missing info");error=true;}
+        if(!error){
+
+            $.ajax({
             type: 'POST',
             url: 'add_form.php',
-            
-            data : {
-            	action : '4',
-            	activity_name : $("#activity_name").val(),
-            	activity_type : $("#activity_type").val(),
-            	other_activity_type : $("#other_activity_type").val(),
-            	activity_location : $("#activity_location").val(), 
-            	other_activity_location : $("#other_activity_location").val(),
-            	activity_organizer : $("#activity_organizer").val(),
-          	    other_activity_organizer_first : $("#other_activity_organizer_first").val(),
-            	other_activity_organizer_last : $("#other_activity_organizer_last").val(),
-            	activity_start_time : $("#activity_start_time").val(),
-            	activity_end_time : $("#activity_end_time").val(),
-            	min_num : $("#min_num").val(),
-            	desired_num : $("#desired_num").val(),
-            	max_num : $("#max_num").val(),
-            	activity_notes : $("#activity_notes").val()
 
-               }//when this is done the msg echoed from the php file that this file posts to which is add_form.php. 
+            data : {
+                action : '4',
+                activity_name : $("#activity_name").val(),
+                activity_type : $("#activity_type").val(),
+                other_activity_type : $("#other_activity_type").val(),
+                activity_location : $("#activity_location").val(),
+                other_activity_location : $("#other_activity_location").val(),
+                activity_organizer : $("#activity_organizer").val(),
+                  other_activity_organizer_first : $("#other_activity_organizer_first").val(),
+                other_activity_organizer_last : $("#other_activity_organizer_last").val(),
+                activity_start_time : $("#activity_start_time").val(),
+                activity_end_time : $("#activity_end_time").val(),
+                min_num : $("#min_num").val(),
+                desired_num : $("#desired_num").val(),
+                max_num : $("#max_num").val(),
+                activity_notes : $("#activity_notes").val()
+
+               }//when this is done the msg echoed from the php file that this file posts to which is add_form.php.
          }).done(function($msg) {
-        
-        	 document.location.href = '../activity.php';});
-    		}
-       
+
+            if($msg.indexOf("nocoveringslot")!= -1 ){bootbox.alert("This activity is created with a time slot OUTSIDE (or partly outside) of the planned conference time. The compatible students shown on the activity page for this activity will not be valid. You will have to find compatible students manually and schedule them.",function() {document.location.href = '../activity.php';});}
+  else{
+  bootbox.alert("activity added.", function() {document.location.href = '../activity.php';});
+ }
+            }
+
     });
  });
--- admin/php/add_form.php
+++ admin/php/add_form.php
@@ -4,11 +4,11 @@ require_once('../../global/include.php');
 
 
  if($_POST["action"]== '1'){
-	$new_activity_type = $_POST["other_activity_type"];
-	$result = $db->query("SELECT * from activity where activity_type='$new_activity_type';");
-	if(mysqli_num_rows($result)!=0){echo "duplicatetype";}
+    $new_activity_type = $_POST["other_activity_type"];
+    $result = $db->query("SELECT * from activity where activity_type='$new_activity_type';");
+    if(mysqli_num_rows($result)!=0){echo "duplicatetype";}
 
-} 
+}
 
 if($_POST["action"]== '2'){
 $new_activity_location = $_POST["other_activity_location"];
@@ -17,38 +17,45 @@ if(mysqli_num_rows($result)!=0){echo "duplicateroom";}
 
 }
 
+ if($_POST["action"]== '5'){
+    $potential_activity_name = $_POST["activity_name"];
+    $result = $db->query("SELECT activity_name from activity where activity_name='$potential_activity_name';");
+    if(mysqli_num_rows($result)!=0){echo "duplicateactivityname";}
 
+}
   if($_POST["action"]== '3'){
-	$new_activity_organizer_first = $_POST["other_activity_organizer_first"];
-	$new_activity_organizer_last = $_POST["other_activity_organizer_last"];
-	$result = $db->query("SELECT * from organizer where first_name='$new_activity_organizer_first' and last_name='$new_activity_organizer_last';");
-	if(mysqli_num_rows($result)!=0){echo "duplicateorganizer";}
+    $new_activity_organizer_first = $_POST["other_activity_organizer_first"];
+    $new_activity_organizer_last = $_POST["other_activity_organizer_last"];
+    $result = $db->query("SELECT * from organizer where first_name='$new_activity_organizer_first' and last_name='$new_activity_organizer_last';");
+    if(mysqli_num_rows($result)!=0){echo "duplicateorganizer";}
 
-}  
+}
 
 
 
 //this $response, which is echoed later goes to the js as msg.
 if($_POST["action"]=='4'){
 $response="failed";
-
+$prev;
 $activity_start_time = $_POST["activity_start_time"];
 $activity_end_time =$_POST["activity_end_time"];
-
+$nu_start=preg_replace("/[^0-9]/", "", $activity_start_time);
+$nu_end=preg_replace("/[^0-9]/", "", $activity_end_time);
 $slot_result = $db->query("select * from time_slots where start_time = '$activity_start_time' and end_time='$activity_end_time';");
 
 //if this time slot does not exist.
 if(mysqli_num_rows($slot_result)!=0){
-	$row = $slot_result->fetch_assoc();
-	$activity_slot_id= $row['slot_id'];
+    $row = $slot_result->fetch_assoc();
+    $activity_slot_id= $row['slot_id'];
 }
 else{
-	$slot_result= $db->query("insert into time_slots(start_time, end_time) values ('$activity_start_time', '$activity_end_time');");
-	$slot_result= $db->query("select * from time_slots where start_time = '$activity_start_time' and end_time='$activity_end_time';");
-	$row=$slot_result->fetch_assoc();
-	$activity_slot_id=$row['slot_id'];
+    $slot_result= $db->query("insert into time_slots(start_time, end_time) values ('$activity_start_time', '$activity_end_time');");
+    $slot_result= $db->query("select * from time_slots where start_time = '$activity_start_time' and end_time='$activity_end_time';");
+    $row=$slot_result->fetch_assoc();
+    $activity_slot_id=$row['slot_id'];
 }
-
+$potential_covering_start= $db->query("select MAX(start_time) as max from time_slots where student_available ='t' and start_time <= ".$nu_start.";");
+    $potential_covering_end= $db->query("select MIN(end_time) as min from time_slots where student_available ='t' and end_time >= ".$nu_end.";");
 
 //get the values posted by ajax in the js file and put them in variables.
 $activity_name = mysqli_real_escape_string($db->getDB(), $_POST["activity_name"]);
@@ -56,31 +63,31 @@ $activity_name = mysqli_real_escape_string($db->getDB(), $_POST["activity_name"]
 if ($_POST["activity_type"]=="Other"){$activity_type = mysqli_real_escape_string($db->getDB(), $_POST["other_activity_type"]);}
 else{$activity_type = mysqli_real_escape_string($db->getDB(), $_POST["activity_type"]);}
 
-if ($_POST["activity_location"]=="Other"){	
+if ($_POST["activity_location"]=="Other"){
 $new_activity_location = $_POST["other_activity_location"];
 $query="INSERT INTO rooms(room_location)
 VALUES ('$new_activity_location');";
 $db->query($query);
 $query="select room_id from rooms where room_location = '$new_activity_location';";
-$result=$db->query($query); 
+$result=$db->query($query);
 $row = $result->fetch_assoc();
 $activity_location_id = $row['room_id'];
 }
 else{$activity_location_id = $_POST["activity_location"];
 }
  if ($_POST["activity_organizer"]=="Other"){
-	$new_activity_organizer_first = $_POST["other_activity_organizer_first"];
-	$new_activity_organizer_last = $_POST["other_activity_organizer_last"];
-	$query="INSERT INTO organizer(first_name,last_name)
-	VALUES ('$new_activity_organizer_first','$new_activity_organizer_last');";
-	$db->query($query);
-	$query="select organizer_id from organizer where first_name = '$new_activity_organizer_first' and last_name='$new_activity_organizer_last';";
-	$result=$db->query($query);
-	$row = $result->fetch_assoc();
-	$activity_organizer_id = $row['organizer_id'];
+    $new_activity_organizer_first = $_POST["other_activity_organizer_first"];
+    $new_activity_organizer_last = $_POST["other_activity_organizer_last"];
+    $query="INSERT INTO organizer(first_name,last_name)
+    VALUES ('$new_activity_organizer_first','$new_activity_organizer_last');";
+    $db->query($query);
+    $query="select organizer_id from organizer where first_name = '$new_activity_organizer_first' and last_name='$new_activity_organizer_last';";
+    $result=$db->query($query);
+    $row = $result->fetch_assoc();
+    $activity_organizer_id = $row['organizer_id'];
 }
 else{$activity_organizer_id = $_POST["activity_organizer"];
-}  
+}
 
 
 
@@ -88,13 +95,13 @@ $activity_start_time = $_POST["activity_start_time"];
 $activity_end_time =mysqli_real_escape_string($db->getDB(), $_POST["activity_end_time"]);
 $min_num =$_POST["min_num"];
 $desired_num =$_POST["desired_num"];
-$max_num =$_POST["max_num"]; 
-$activity_notes =mysqli_real_escape_string($db->getDB(), $_POST["activity_notes"]); 
+$max_num =$_POST["max_num"];
+$activity_notes =mysqli_real_escape_string($db->getDB(), $_POST["activity_notes"]);
 
 
 //query the database and insert a new record of activity with these input values.
 $query = "INSERT INTO activity(room_id, slot_id, activity_name, activity_type,
- min_workers,desired_workers,max_workers,activity_notes) VALUES 
+ min_workers,desired_workers,max_workers,activity_notes) VALUES
 ('$activity_location_id','$activity_slot_id', '$activity_name', '$activity_type',
 '$min_num','$desired_num','$max_num','$activity_notes')";
 $db->query($query);
@@ -105,7 +112,7 @@ $row = $result->fetch_assoc();
 $activity_id = $row['activity_id'];
  $query="INSERT INTO organizer_activity(activity_id,organizer_id)
 VALUES ('$activity_id','$activity_organizer_id');";
-$db->query($query);   
+$db->query($query);
 
 $query="select instruction_id
 from instructions
@@ -113,12 +120,49 @@ where instruction_type like '%$activity_type%'";
 $db->query($query);
 $result=$db->query($query);
 if(mysqli_num_rows($result)!=0){
-	$row = $result->fetch_assoc();	
-	$instruction_id = $row['instruction_id'];
-	$query="insert into activity_instructions (activity_id, instruction_id)
+    $row = $result->fetch_assoc();
+    $instruction_id = $row['instruction_id'];
+    $query="insert into activity_instructions (activity_id, instruction_id)
 values ('$activity_id','$instruction_id');";
 $db->query($query);}
 
+$covering_start_row=$potential_covering_start->fetch_assoc();
+    $covering_start=$covering_start_row["max"];
+    $covering_end_row=$potential_covering_end->fetch_assoc();
+    $covering_end=$covering_end_row["min"];
+if($covering_start!=null && $covering_end!=null){
+
+
+    $nu_covering_start=preg_replace("/[^0-9]/", "", $covering_start);
+    $nu_covering_end=preg_replace("/[^0-9]/", "", $covering_end);
+    $prev=$covering_start;
+    $covering_slots_result=$db->query("select slot_id, start_time, end_time from time_slots where student_available ='t' and start_time >= ".$nu_covering_start." and end_time <= ".$nu_covering_end.";");
+$covering_slots_affected_rows = mysqli_num_rows($covering_slots_result);
+
+
+                    for ($i = 0; $i < $covering_slots_affected_rows; $i++) {
+                                 $covering_slots_row=$covering_slots_result->fetch_assoc();
+                                $covering_slot=$covering_slots_row["slot_id"];
+
+                                if($covering_slots_row["start_time"]!=$prev){
+                                    echo"nocoveringslot";
+                                    break;
+                                }
+                                else{$add_covering_slot= $db->query("insert into covering_time_slots(activity_id, slot_id) values ('$activity_id', '$covering_slot');");
+                                    $prev=$covering_slots_row["end_time"];
+                            }
+
+
+
+                         } }
+                         else{
+                             echo "nocoveringslot";
+                         }
+
+
 }
+
+
+
 
 ?>
